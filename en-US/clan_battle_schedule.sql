b616f2b6805b69ff6e6a9dc6e7af057eaa6d4a8a
Dimbreath-Server <dimbreath+1@gmail.com>
2021-05-20 08:00:41 +0000

    ResVersion: 10001310

--- en-US/clan_battle_schedule.sql
+++ en-US/clan_battle_schedule.sql
@@ -3,5 +3,5 @@ CREATE TABLE 'clan_battle_schedule' ('clan_battle_id' INTEGER NOT NULL, 'release
 INSERT INTO "clan_battle_schedule" VALUES(1001,2,0,1,1,'bgm_M90',1,'2021/02/10 13:00:00','2021/03/06 12:59:59');
 INSERT INTO "clan_battle_schedule" VALUES(1002,3,1001,1,1,'bgm_M97',2,'2021/03/07 13:00:00','2021/04/01 12:59:59');
 INSERT INTO "clan_battle_schedule" VALUES(1003,4,1002,1,1,'bgm_M104',1,'2021/04/12 13:00:00','2021/05/19 12:59:59');
-INSERT INTO "clan_battle_schedule" VALUES(1004,6,1003,1,1,'bgm_M115',2,'2021/05/19 13:00:00','2021/06/22 12:59:59');
+INSERT INTO "clan_battle_schedule" VALUES(1004,5,1003,1,1,'bgm_M115',2,'2021/05/19 13:00:00','2021/06/22 12:59:59');
 COMMIT;
